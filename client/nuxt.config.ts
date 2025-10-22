d47740fd3302c3bc76697e30ec46618bda224769
cribertt <riberttcss23@gmail.com>
2025-10-22 14:15:46 -0300

    Update nuxt.config.ts

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+   app: {
+    baseURL: '/inacap/',     // 👈 MUY importante el slash inicial y final
+    buildAssetsDir: '/_nuxt/' // opcional; con slash inicial (default ya es /_nuxt/)
+  },
+  devtools: { enabled: true },
+  ssr: false, 
 })
